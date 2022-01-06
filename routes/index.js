2a455bc66c1c246cf77c749d5b2fa18479a91efb
Alan Sanchez <alnwolff@gmail.com>
2022-01-06 19:36:33 +0100

    add CREATE functionality on API

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,37 @@
+const Project = require("../models/Project");
+
 const router = require("express").Router();
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
+});
+
+router.post("/add", (req, res, next) => {
+  const {
+    name,
+    techUsed,
+    releaseDate,
+    credit,
+    screenshotUrl,
+    videoUrl,
+    details,
+  } = req.body;
+
+  Project.create({
+    name,
+    techUsed,
+    releaseDate,
+    credit,
+    screenshotUrl,
+    videoUrl,
+    details,
+  })
+    .then((createdProject) => {
+      return res.status(200).json(createdProject);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 // You put the next routes here 👇
